0a1fafa0b40d9f6739f3ca5e9398c557a7c4863d
iseedouble <tsteven@live.ca>
2024-07-18 13:13:16 -0400

    remove audio

--- data/audio.js
+++ data/audio.js
@@ -2,36 +2,36 @@ const audio = {
   Map: new Howl({
     src: './audio/map.wav',
     html5: true,
-    volume: 0.1
+    volume: 0.0
   }),
   initBattle: new Howl({
     src: './audio/initBattle.wav',
     html5: true,
-    volume: 0.1
+    volume: 0.0
   }),
   battle: new Howl({
     src: './audio/battle.mp3',
     html5: true,
-    volume: 0.1
+    volume: 0.0
   }),
   tackleHit: new Howl({
     src: './audio/tackleHit.wav',
     html5: true,
-    volume: 0.1
+    volume: 0.0
   }),
   fireballHit: new Howl({
     src: './audio/fireballHit.wav',
     html5: true,
-    volume: 0.1
+    volume: 0.0
   }),
   initFireball: new Howl({
     src: './audio/initFireball.wav',
     html5: true,
-    volume: 0.1
+    volume: 0.0
   }),
   victory: new Howl({
     src: './audio/victory.wav',
     html5: true,
-    volume: 0.1
+    volume: 0.0
   })
 }
